8b5e68d1a1381864659fa1a08bf2113f8c94a61e
milypferraresso <mily.p.ferraresso@gmail.com>
2026-05-20 12:29:29 -0300

    estrutura-de-dados

---
+++ script.js
@@ -0,0 +1,30 @@
+const alunos = [
+    { 
+        nome: "Amanda",
+        idade: 15,
+        disciplinaPreferida: "História"
+},
+{
+    nome: "Matheus",
+    idade: 16,
+    disciplinaPreferida: "Matemática"
+},
+{
+    nome: "Guilerme",
+    idade: 17,
+    disciplinaPreferida: "Educação Fisíca"
+},
+{
+    nome: "Julía",
+    idade: 15,
+    disciplinaPreferida: "Português",
+},
+{
+    nome: "Lavínia",
+    idade: 16,
+    disciplinaPreferida: "Química"
+}
+];
+
+alert(alunos[0].nome);
+alert(alunos[4].disciplinaPreferida);
